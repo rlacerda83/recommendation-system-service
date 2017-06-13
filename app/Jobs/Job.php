4e44e2fe7a12684e4d4938596024abdc2b648ca1
Rodrigo Lacerda Pereira <rodrigo.pereira@mobly.com.br>
2017-06-13 18:31:30 -0300

    fix job interface

--- app/Jobs/Job.php
+++ app/Jobs/Job.php
@@ -3,12 +3,11 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-abstract class Job implements SelfHandling, ShouldQueue
+abstract class Job implements ShouldQueue
 {
     /*
     |--------------------------------------------------------------------------
